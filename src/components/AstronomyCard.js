a4a2924f8944044a5451d6912ec15a1d5468c7e0
ranraok <46853842+ranraok@users.noreply.github.com>
2020-02-20 19:11:46 -0500

    began playing around with the bootstrap and reactstrap systems and adjusted some text color and image size, as well as changed the wireframing for the AstronomyCard

--- src/components/AstronomyCard.js
+++ src/components/AstronomyCard.js
@@ -3,9 +3,22 @@ import Dates from "./Dates"
 import axios from 'axios'
 import Title from "./Title"
 import Image from "./Image"
-import description from './Description'
 import Description from "./Description"
+import styled from "styled-components"
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Button
+} from 'reactstrap';
 
+
+const AstoButton = styled.button`
+
+  width: 200px;
+  height: 50px;
+  background: purple;
+  color: yellow;
+
+`
 
 const AstronomyCard = props => {
 
@@ -23,14 +36,27 @@ const AstronomyCard = props => {
     },[])
 
     return (
-        <div className="astro-card" key={props.id}>
+        /*<div className="astro-card" key={props.id}>
             <div className = 'entry'>
                     <Dates date={nasaData.date}/>
                     <Title title={nasaData.title}/>
                     <Image key={nasaData.id} url={nasaData.url}/>
                     <Description explanation={nasaData.explanation}/>
             </div>
-        </div>
+        </div>*/
+
+       
+         <Card>
+              <CardBody>
+                <CardSubtitle>Today's Date: {nasaData.date}</CardSubtitle>
+                <CardTitle>Card title; {nasaData.title}</CardTitle>
+                <CardImg width="35%" src={nasaData.url}></CardImg>
+                <CardText style = {{ width: "50%", margin: "auto", color: "red"}}>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
+                <Button>Button</Button>
+              </CardBody>
+         </Card>
+         
+
     );
 };
 
